8cbfc44d87bad3931d5f85ed3664f53e75642c24
Ethan Rogers <ethanfrogers@gmail.com>
2014-06-11 15:35:16 -0400

    styling updates

--- js/skills.js
+++ js/skills.js
@@ -1,6 +1,8 @@
 $('document').ready(function(){
-	$("#resume_list li").click(function(event){
-		var item = $(event.target).attr('item');
+	$("#resume_list button").click(function(event){
+		var item = $(event.target).parents('li').attr('item');
+		$("#resume_list").find('button').removeClass('btn-primary');
+		$(event.target).addClass('btn-primary');
 		$("#resume_content").find('.content').hide();
 		$("#resume_content").find('#' + item).slideDown(500);
 	});
